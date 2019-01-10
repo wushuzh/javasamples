0205358b8b4859127067e24ae01924a9f7a34475
wushuzh <zhangjiaqiang@gmail.com>
2019-01-10 15:20:12 +0800

    Add poplibs and its child project json

---
+++ poplibs/json/src/main/java/io/github/wushuzh/App.java
@@ -0,0 +1,13 @@
+package io.github.wushuzh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
