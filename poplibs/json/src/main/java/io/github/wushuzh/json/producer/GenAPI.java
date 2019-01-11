e065bb5b9b45fafdca75ba092c276a851021d55b
wushuzh <zhangjiaqiang@gmail.com>
2019-01-11 11:34:04 +0800

    Add top level json elements

---
+++ poplibs/json/src/main/java/io/github/wushuzh/json/producer/GenAPI.java
@@ -0,0 +1,34 @@
+package io.github.wushuzh.json.producer;
+
+import java.io.IOException;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import io.github.wushuzh.json.pojo.ExampleLoan;
+import io.github.wushuzh.json.pojo.LoanApplication;
+
+public class GenAPI {
+  public static void main(String[] args) throws IOException {
+    LoanApplication loanApplication = ExampleLoan.LOAN_APPLICATION;
+    System.out.println(loanApplication);
+    System.out.println();
+    toJsonString(loanApplication);
+
+  }
+
+  private static void toJsonString(LoanApplication loanApplication) throws IOException {
+
+    JsonFactory jsonFactory = new JsonFactory();
+    JsonGenerator jsonGenerator = jsonFactory.createGenerator(System.out);
+    jsonGenerator.setPrettyPrinter(new DefaultPrettyPrinter());
+
+    jsonGenerator.writeStartObject();
+    
+    jsonGenerator.writeStringField("name", loanApplication.getName());
+    jsonGenerator.writeStringField("purposeOfLoan", loanApplication.getPurposeOfLoan());
+    
+    jsonGenerator.writeEndObject();
+    jsonGenerator.flush();
+  }
+
+}
