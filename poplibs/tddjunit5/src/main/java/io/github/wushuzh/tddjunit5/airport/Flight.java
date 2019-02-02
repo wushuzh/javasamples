f1cd66098a4866930055d0ff17c7c4a6ab293adf
wushuzh <zhangjiaqiang@gmail.com>
2019-02-02 14:54:51 +0800

    Add init airport app used for junit5 tdd learning

---
+++ poplibs/tddjunit5/src/main/java/io/github/wushuzh/tddjunit5/airport/Flight.java
@@ -0,0 +1,48 @@
+package io.github.wushuzh.tddjunit5.airport;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Flight {
+  private String id;
+  private List<Passenger> passengersList = new ArrayList<Passenger>();
+  private String flightType;
+  
+  public Flight(String id, String flightType) {
+    this.id = id;
+    this.flightType = flightType;
+  }
+  
+  public String getId() { return id; }
+  public String getFlightType() { return flightType; }
+  public List<Passenger> getPassengersList() { return Collections.unmodifiableList(passengersList); }
+  
+  public boolean addPassenger(Passenger passenger) {
+    switch (flightType) {
+      case "Economy":
+        return passengersList.add(passenger);
+      case "Business":
+        if (passenger.isVip()) {
+          return passengersList.add(passenger);
+        }
+        return false;
+      default:
+        throw new RuntimeException("Unknow type: " + flightType);
+    }
+  }
+  
+  public boolean removePassenger(Passenger passenger) {
+    switch (flightType) {
+      case "Economy":
+        if (passenger.isVip()) {
+          return passengersList.remove(passenger);
+        }
+        return false;
+      case "Business":
+        return false;
+      default:
+        throw new RuntimeException("Unknown type: " + flightType);
+    }
+  }
+}
