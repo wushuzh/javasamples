b8984ddac3b71bdb3f2d69514096dfa00b916201
wushuzh <zhangjiaqiang@gmail.com>
2019-02-03 08:18:06 +0800

    Fix bugs for economy Flight rm with junit5

--- poplibs/tddjunit5/src/main/java/io/github/wushuzh/tddjunit5/airport/Flight.java
+++ poplibs/tddjunit5/src/main/java/io/github/wushuzh/tddjunit5/airport/Flight.java
@@ -8,16 +8,24 @@ public class Flight {
   private String id;
   private List<Passenger> passengersList = new ArrayList<Passenger>();
   private String flightType;
-  
+
   public Flight(String id, String flightType) {
     this.id = id;
     this.flightType = flightType;
   }
-  
-  public String getId() { return id; }
-  public String getFlightType() { return flightType; }
-  public List<Passenger> getPassengersList() { return Collections.unmodifiableList(passengersList); }
-  
+
+  public String getId() {
+    return id;
+  }
+
+  public String getFlightType() {
+    return flightType;
+  }
+
+  public List<Passenger> getPassengersList() {
+    return Collections.unmodifiableList(passengersList);
+  }
+
   public boolean addPassenger(Passenger passenger) {
     switch (flightType) {
       case "Economy":
@@ -31,11 +39,11 @@ public class Flight {
         throw new RuntimeException("Unknow type: " + flightType);
     }
   }
-  
+
   public boolean removePassenger(Passenger passenger) {
     switch (flightType) {
       case "Economy":
-        if (passenger.isVip()) {
+        if (!passenger.isVip()) {
           return passengersList.remove(passenger);
         }
         return false;
---
+++ poplibs/tddjunit5/src/test/java/io/github/wushuzh/tddjunit5/airport/AirportTest.java
@@ -0,0 +1,26 @@
+package io.github.wushuzh.tddjunit5.airport;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AirportTest {
+
+  @Test
+  public void testAirport() {
+    Flight economyFlight = new Flight("1", "Economy");
+    Flight businessFlight = new Flight("2", "Business");
+
+    Passenger john = new Passenger("John", true);
+    Passenger mike = new Passenger("Mike", false);
+
+    assertEquals(true, economyFlight.addPassenger(john));
+    assertEquals(false, economyFlight.removePassenger(john));
+    assertEquals(true, businessFlight.addPassenger(john));
+    assertEquals(false, businessFlight.removePassenger(john));
+
+    assertEquals(true, economyFlight.addPassenger(mike));
+    assertEquals(true, economyFlight.removePassenger(mike));
+    assertEquals(false, businessFlight.addPassenger(mike));
+    assertEquals(false, businessFlight.removePassenger(mike));
+  }
+}
