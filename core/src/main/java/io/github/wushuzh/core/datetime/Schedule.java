e89a6d4469f9c8753d22390776dbb814af000076
wushuzh <zhangjiaqiang@gmail.com>
2019-02-14 14:16:40 +0800

    Add toString to printWP in finalized scheduling
    1. In case some genius defines the regular workperiod across midnight,
    we make a decision to split it as two workperiods (one new WorkPeriod
    and the original but mutated WorkPeriod and then merge) under each day
    when printing.
    
    2. put all workperiods in navMap as sorted, whose key is startTime,
    value is String repr of workperiod, inlcuding all taskParts within it.
    
    3. put step2 navMap into another navMap, whose key is startDay, value is
    String is all workperiods within same day

--- core/src/main/java/io/github/wushuzh/core/datetime/Schedule.java
+++ core/src/main/java/io/github/wushuzh/core/datetime/Schedule.java
@@ -1,8 +1,19 @@
 package io.github.wushuzh.core.datetime;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Map;
 import java.util.NavigableSet;
+import java.util.Optional;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.joining;
 
 public class Schedule {
 
@@ -24,5 +35,33 @@ public class Schedule {
 
   public boolean isSuccessful() {
     return successful;
+  }
+
+  @Override
+  public String toString() {
+    if (!successful) return "Schedule unsuccessful: insufficent time for tasks";
+
+    List<WorkPeriod> printablePeriods = scheduledPeriods.stream().map(WorkPeriod::copy).collect(toList());
+    List<WorkPeriod> periodsSplitByMidnight = printablePeriods.stream()
+      .map(WorkPeriod::split)
+      .filter(Optional::isPresent)
+      .map(Optional::get)
+      .collect(toList());
+
+    printablePeriods.addAll(periodsSplitByMidnight);
+
+    NavigableMap<LocalDateTime, String> dateTimeToPeriodOutput = printablePeriods.stream()
+      .collect(groupingBy(WorkPeriod::getStartTime, TreeMap::new, mapping(WorkPeriod::toString, joining())));
+
+    NavigableMap<LocalDate, String> dateToCalendarObjectOutput = dateTimeToPeriodOutput.entrySet().stream()
+      .collect(groupingBy(e -> e.getKey().toLocalDate(), TreeMap::new, mapping(Map.Entry::getValue, joining())));
+
+    StringBuilder sb = new StringBuilder();
+    for (LocalDate date : dateToCalendarObjectOutput.keySet()) {
+      sb.append("\n");
+      sb.append(date);
+      sb.append(dateToCalendarObjectOutput.get(date));
+    }
+    return sb.toString();
   }
 }
