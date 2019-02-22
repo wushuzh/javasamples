b158cc4e5f90d9493af5296ac60bd4948d26fd27
wushuzh <zhangjiaqiang@gmail.com>
2019-02-22 09:34:27 +0800

    Refactor a common method for processEnquiry
    Using java8 lambda function as a Predicate arg

--- core/src/main/java/io/github/wushuzh/core/collections/CategoriesedHelpDesk.java
+++ core/src/main/java/io/github/wushuzh/core/collections/CategoriesedHelpDesk.java
@@ -2,6 +2,7 @@ package io.github.wushuzh.core.collections;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 public class CategoriesedHelpDesk {
   private final Queue<Enquiry> enquiries = new ArrayDeque<>();
@@ -23,24 +24,26 @@ public class CategoriesedHelpDesk {
     }
   }
 
-  public void processPrinterEnquiry() {
-    Enquiry enquiry = enquiries.peek();
-    if (enquiry != null && enquiry.getCategory() == Category.PRINTER) {
+  private void processEnquiry(final Predicate<Enquiry> predicate, final String msg) {
+    final Enquiry enquiry = enquiries.peek();
+    if (enquiry != null & predicate.test(enquiry)) {
+      enquiry.getCustomer().reply(msg);
       enquiries.remove();
-      enquiry.getCustomer().reply("Is it out of paper?");
     } else {
       System.out.println("No work to do, let's have some coffee!");
     }
   }
 
+  public void processPrinterEnquiry() {
+    processEnquiry(
+        enq -> enq.getCategory() == Category.PRINTER,
+        "Is it out of paper");
+  }
+
   public void processOtherEnquiry() {
-    Enquiry enquiry = enquiries.peek();
-    if (enquiry != null && enquiry.getCategory() != Category.PRINTER) {
-      enquiries.remove();
-      enquiry.getCustomer().reply("Have you tried turning it off and on again?");
-    } else {
-      System.out.println("No work to do, let's have some coffee!");
-    }
+    processEnquiry(
+        enq -> enq.getCategory() != Category.PRINTER,
+        "Have you tried turning it off and on again?");
   }
 
   public static void main(String[] args) {
