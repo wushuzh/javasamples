3ccf4998f19f3ff721c8c6db218858774fd8e3d4
wushuzh <zhangjiaqiang@gmail.com>
2019-02-22 08:54:46 +0800

    Impl tickets input output handling using Queue
    For put element into the Queue, recommend to use offer, since it return
    false instead of causing exception when Queue is full. (compare with
    add)
    For get element out of Queue,
    1. use remove when Queue is not empty
    2. use poll and double check returning is not null

--- core/src/main/java/io/github/wushuzh/core/collections/HelpDesk.java
+++ core/src/main/java/io/github/wushuzh/core/collections/HelpDesk.java
@@ -1,12 +1,25 @@
 package io.github.wushuzh.core.collections;
 
-public class HelpDesk {
-  public void enquire(final Customer customer, final Category category) {
+import java.util.ArrayDeque;
+import java.util.Queue;
 
+public class HelpDesk {
+  private final Queue<Enquiry> enquiries = new ArrayDeque<>();
+
+  public void enquire(final Customer customer, final Category category) {
+    enquiries.offer(new Enquiry(customer, category));
   }
 
   public void processAllEnquiries() {
+//    while (!enquiries.isEmpty()) {
+//      final Enquiry enquiry = enquiries.remove();
+//      enquiry.getCustomer().reply("Have U tried turning it off and on again?");
+//    }
 
+    Enquiry enquiry;
+    while ((enquiry = enquiries.poll()) != null) {
+      enquiry.getCustomer().reply("Have you tried turning it off and on again?");
+    }
   }
 
   public static void main(String[] args) {
