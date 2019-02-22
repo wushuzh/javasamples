52ba071520b8433955ec6a4354a4e008ac386c69
wushuzh <zhangjiaqiang@gmail.com>
2019-02-22 09:54:45 +0800

    Use Priority Queue to decide answer ordering
    1. fix bug to short circuit using double ampersand
    2. provide a comparator to PriorityQueue constructor

--- core/src/main/java/io/github/wushuzh/core/collections/CategoriesedHelpDesk.java
+++ core/src/main/java/io/github/wushuzh/core/collections/CategoriesedHelpDesk.java
@@ -1,11 +1,19 @@
 package io.github.wushuzh.core.collections;
 
-import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.function.Predicate;
 
 public class CategoriesedHelpDesk {
-  private final Queue<Enquiry> enquiries = new ArrayDeque<>();
+
+  private static final Comparator<Enquiry> BY_CATEGORY = new Comparator<Enquiry>() {
+    @Override
+    public int compare(Enquiry o1, Enquiry o2) {
+      return o1.getCategory().compareTo(o2.getCategory());
+    }
+  };
+  private final Queue<Enquiry> enquiries = new PriorityQueue<>(BY_CATEGORY);
 
   public void enquire(final Customer customer, final Category category) {
     enquiries.offer(new Enquiry(customer, category));
@@ -20,13 +28,14 @@ public class CategoriesedHelpDesk {
 
     Enquiry enquiry;
     while ((enquiry = enquiries.poll()) != null) {
-      enquiry.getCustomer().reply("Have you tried turning it off and on again?");
+      enquiry.getCustomer().reply(
+          String.format("Have you tried turning %s off and on again?", enquiry.getCategory()));
     }
   }
 
   private void processEnquiry(final Predicate<Enquiry> predicate, final String msg) {
     final Enquiry enquiry = enquiries.peek();
-    if (enquiry != null & predicate.test(enquiry)) {
+    if (enquiry != null && predicate.test(enquiry)) {
       enquiry.getCustomer().reply(msg);
       enquiries.remove();
     } else {
@@ -50,9 +59,17 @@ public class CategoriesedHelpDesk {
     CategoriesedHelpDesk helpDesk = new CategoriesedHelpDesk();
 
     helpDesk.enquire(Customer.JACK, Category.PHONE);
+    helpDesk.enquire(Customer.JACK, Category.PRINTER);
     helpDesk.enquire(Customer.JILL, Category.PRINTER);
+    helpDesk.enquire(Customer.MARY, Category.COMPUTER);
 
-//    helpDesk.processAllEnquiries();
+    helpDesk.processAllEnquiries();
+// Priority order is Printer -> Computer -> PHONE -> TABLET
+//    Jack: Have you tried turning PRINTER off and on again?
+//    Jill: Have you tried turning PRINTER off and on again?
+//    Mary: Have you tried turning COMPUTER off and on again?
+//    Jack: Have you tried turning PHONE off and on again?
+
     helpDesk.processPrinterEnquiry();
     helpDesk.processOtherEnquiry();
     helpDesk.processPrinterEnquiry();
