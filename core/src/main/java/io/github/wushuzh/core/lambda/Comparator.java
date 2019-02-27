ccc3e2379c967ac76c0ddf2141f8169ec931caa6
wushuzh <zhangjiaqiang@gmail.com>
2019-02-27 17:30:01 +0800

    Refactor age Comparator in a more common way
    1. Create a function f3 takes in a Person and return an int, the common
    patterns of all 3 comparator is to take in a Person and return one
    field.
    2. Create a static function in itf with a lambda function to invoke the
    f3 function with two Person and compare

--- core/src/main/java/io/github/wushuzh/core/lambda/Comparator.java
+++ core/src/main/java/io/github/wushuzh/core/lambda/Comparator.java
@@ -1,6 +1,12 @@
 package io.github.wushuzh.core.lambda;
 
+import java.util.function.Function;
+
 @FunctionalInterface
 public interface Comparator<T> {
   public int compare(T t1, T t2);
+
+  public static Comparator<Person> comparing(Function<Person, Integer> f) {
+    return (p1, p2) -> f.apply(p2) - f.apply(p1);
+  }
 }
