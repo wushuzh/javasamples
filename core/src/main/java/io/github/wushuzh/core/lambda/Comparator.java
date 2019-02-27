bd232f349b569765e0f4c56308397d99db2bb6e4
wushuzh <zhangjiaqiang@gmail.com>
2019-02-27 17:41:36 +0800

    Refactor Comparator static comparing more generic
    1. Change comparing arg type as a function, which still takes in a
    Person but return a Comparable
    2. int is automatically boxing as an Integer, which is Comparable, so as
    String

--- core/src/main/java/io/github/wushuzh/core/lambda/Comparator.java
+++ core/src/main/java/io/github/wushuzh/core/lambda/Comparator.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 public interface Comparator<T> {
   public int compare(T t1, T t2);
 
-  public static Comparator<Person> comparing(Function<Person, Integer> f) {
-    return (p1, p2) -> f.apply(p2) - f.apply(p1);
+  public static Comparator<Person> comparing(Function<Person, Comparable> f) {
+    return (p1, p2) -> f.apply(p2).compareTo(f.apply(p1));
   }
 }
--- core/src/main/java/io/github/wushuzh/core/lambda/MainComparator.java
+++ core/src/main/java/io/github/wushuzh/core/lambda/MainComparator.java
@@ -1,14 +1,12 @@
 package io.github.wushuzh.core.lambda;
 
-import java.util.function.Function;
-
 public class MainComparator {
   public static void main(String[] args) {
     Comparator<Person> cmpFirstName = (p1, p2) -> p1.getFirstName().compareTo(p2.getFirstName());
     Comparator<Person> cmpLastName = (p1, p2) -> p1.getLastName().compareTo(p2.getLastName());
     Comparator<Person> cmpAge = (p1, p2) -> p2.getAge() - p1.getAge();
 
-    Function<Person, Integer> f3 = p -> p.getAge();
-    Comparator<Person> cmpPersonByAge = Comparator.comparing(f3);
+    Comparator<Person> cmpPersonByAge = Comparator.comparing(Person::getAge);
+    Comparator<Person> cmpPersonByFirstName = Comparator.comparing(Person::getFirstName);
   }
 }
