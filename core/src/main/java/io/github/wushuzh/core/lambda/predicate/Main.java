e47214fe3f7d3060b93a365c12098b034971bcc8
wushuzh <zhangjiaqiang@gmail.com>
2019-03-06 10:26:04 +0800

    Add default methods for chaining
    The default methods could be added as long as they make scene in the
    domain of Predicate
    1. "and" chains another Predicate and Return a lambda Predicate
    2. "or" do the same following logic ops

--- core/src/main/java/io/github/wushuzh/core/lambda/predicate/Main.java
+++ core/src/main/java/io/github/wushuzh/core/lambda/predicate/Main.java
@@ -5,10 +5,26 @@ package io.github.wushuzh.core.lambda.predicate;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        Predicate<String> p = s -> s.length() < 20;
+  public static void main(String[] args) {
+    Predicate<String> p1 = s -> s.length() < 20;
+    Predicate<String> p2 = s -> s.length() > 5;
 
-        boolean b = p.test("Hello");
-        System.out.println("Hello is shorter than 20 charts: " + b);
-    }
+    Predicate<String> p3 = p1.and(p2);
+
+    System.out.println("p3 for Yes: " + p3.test("Yes"));
+    System.out.println("p3 for Good morning: " + p3.test("Good morning"));
+    System.out.println("p3 for Good morning gentlemen: " + p3.test("Good morning gentlemen"));
+    // p3 for Yes: false
+    // p3 for Good morning: true
+    // p3 for Good morning gentlemen: false
+
+    Predicate<String> p4 = p1.or(p2);
+
+    System.out.println("p4 for Yes: " + p4.test("Yes"));
+    System.out.println("p4 for Good morning: " + p4.test("Good morning"));
+    System.out.println("p4 for Good morning gentlemen: " + p4.test("Good morning gentlemen"));
+    // p4 for Yes: true
+    // p4 for Good morning: true
+    // p4 for Good morning gentlemen: true
+  }
 }
--- core/src/main/java/io/github/wushuzh/core/lambda/predicate/Predicate.java
+++ core/src/main/java/io/github/wushuzh/core/lambda/predicate/Predicate.java
@@ -5,5 +5,13 @@ package io.github.wushuzh.core.lambda.predicate;
  */
 @FunctionalInterface
 public interface Predicate<T> {
-    public boolean test(T t);
+  public boolean test(T t);
+
+  public default Predicate<T> and(Predicate<T> other) {
+    return t -> test(t) && other.test(t);
+  }
+
+  public default Predicate<T> or(Predicate<T> other) {
+    return t -> test(t) || other.test(t);
+  }
 }
